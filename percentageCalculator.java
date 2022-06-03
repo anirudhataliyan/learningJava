f8cffef86e876f215727399d3a9eda1c256da52e
Anirudha Taliyan <akt746083@gmail.com>
2022-06-03 22:09:31 +0530

    Update percentageCalculator.java

--- percentageCalculator.java
+++ percentageCalculator.java
@@ -18,31 +18,7 @@ class Main{
 	float sum = hindi+eng+maths+sci+sst;
 	float percentage = ((sum/500)*100);
 	System.out.print("The total percentage is: ");
-	System.out.printlpackage com.company; 
-import java.util.Scanner;
-
-class Main{
-    public static void main(String args[]) {
-        System.out.println("*** Welcome to percentage Calculator ***");
-	Scanner sc = new Scanner(System.in);
-	System.out.print("Enter marks of Hindi: ");
-	float hindi = sc.nextFloat();
-       	System.out.print("Enter marks of English: ");
-	float eng = sc.nextFloat();
-        System.out.print("Enter marks of Maths: ");
-        float maths = sc.nextFloat();
-        System.out.print("Enter marks of Science: ");
-        float sci = sc.nextFloat();
-	System.out.print("Enter marks of Social Science: ");
-	float sst = sc.nextFloat();
-	float sum = hindi+eng+maths+sci+sst;
-	float percentage = ((sum/500)*100);
-	System.out.print("The total percentage is: ");
 	System.out.println(percentage);		
-    }
-}
-
-n(percentage);		
     }
 }
 
